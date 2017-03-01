72a2e92f23f84c2033b4e00470f6de37d97877b3
MrHertz <rus.syndicate@gmail.com>
2017-03-01 11:26:08 +0200

    test

--- ajax/basket/basket.php
+++ ajax/basket/basket.php
@@ -35,9 +35,9 @@ function add_to_cart($product_id, $count = 1)
 
 
     return echo json_encode([
-        "id" => $product_id,
-        "count" => $_SESSION['products'][$product_id]['count'],
-        "coast" => $_SESSION['products'][$product_id]['coast'],
+        "id" => $product_id//,
+        //"count" => $_SESSION['products'][$product_id]['count'],
+        //"coast" => $_SESSION['products'][$product_id]['coast'],
         //"all" => update_cart()
     ]);
 }
