2617a1af673b2d75a6938e2992808b9e7e9816e2
MrHertz <rus.syndicate@gmail.com>
2017-03-01 11:00:56 +0200

    test

--- ajax/basket/basket.php
+++ ajax/basket/basket.php
@@ -1,5 +1,15 @@
 <?
-include $_SERVER['DOCUMENT_ROOT'] . '_config.php';
+$dbuser  = "antondtl_sm";
+$dbname  = "antondtl_sm";
+$dbpass  = "y3l0l3k0r";
+$dbhost  = "antondtl.beget.tech";
+
+mysql_connect($dbhost, $dbuser, $dbpass) or die ("Could not connect: ".mysql_error());
+mysql_select_db($dbname);
+mysql_query("set names utf8");
+
+session_start();
+
 
 if (isset($_POST['add_basket_item'])) {
     add_to_cart($_POST['add_basket_item']);
