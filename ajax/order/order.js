13339e857ea36b5dbb37aa35598c97be54ee0159
MrHertz <rus.syndicate@gmail.com>
2017-03-06 14:18:58 +0300

    logo

--- ajax/order/order.js
+++ ajax/order/order.js
@@ -8,9 +8,11 @@ var order = {
 			document.querySelector('#min_order_coast').style.display = 'block';
 
 			let arr = fullAddr.split(',');
-			console.log(fullAddr);
-			console.log(arr);
-			//document.querySelector('#min_order_coast').innerText = arr.[arr.length-1] + ' ' + arr.[arr.length] + ': ' + addr.hintContent; //ТЕКСТ О МИН ЗАКАЗЕ
+			let str = '';
+			for (let i = 2; i < arr.length; i++) {
+				str += arr[i] + ' ';
+			}
+			document.querySelector('#min_order_coast').innerText = ': ' + addr.hintContent; //ТЕКСТ О МИН ЗАКАЗЕ
 
 			if (order.coasts[addr.id] <= parseInt(document.querySelector('#order_all_coast').innerText, 10)) {
 				document.querySelector('#order_submit').removeAttribute('disabled');
