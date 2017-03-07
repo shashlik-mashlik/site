f24529afab6a34321f045b9f8b8a3e213b5685e9
MrHertz <rus.syndicate@gmail.com>
2017-03-07 15:57:41 +0300

    -

--- admin/module/orders/edit.php
+++ admin/module/orders/edit.php
@@ -1,11 +1,10 @@
 <?
-echo $_GET['id'];
 $sql = "SELECT `status` FROM `orders` WHERE `id` = " . $_GET['id'];
 $r = mysql_fetch_assoc(mysql_query($sql));
 if ($r['status'] == 'ready') {
-    mysql_query("UPDATE `orders` SET `status` = 'expected'");
+    mysql_query("UPDATE `orders` SET `status` = 'expected' WHERE `id`=" . $_GET['id']);
 } else {
-    mysql_query("UPDATE `orders` SET `status` = 'sent'");
+    mysql_query("UPDATE `orders` SET `status` = 'sent' WHERE `id`=" . $_GET['id']);
 }
 Header('Location: /admin/module/orders');
 ?>
