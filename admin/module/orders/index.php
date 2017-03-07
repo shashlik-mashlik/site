8814be232b5ad2eb915d0245ee23bab2074d4258
MrHertz <rus.syndicate@gmail.com>
2017-03-07 14:57:07 +0300

    -

--- admin/module/orders/index.php
+++ admin/module/orders/index.php
@@ -46,15 +46,15 @@ else {
 	$r = mysql_query($sql) or die('DB ERROR: CAN\'T EXTRACT orders');
 	for($data=array();$row=mysql_fetch_assoc($r);$data[]=$row);
 
-	$html = "<table>";
-    $ord = json_decode($r['ord']);
-	for ($i = 0; $i < sizeof($ord); $i++) {
-	    $html .= "<tr><td>";
-	    $html .= $ord[$i]->name . ' ' . $ord[$i]->count . ' ' . $ord[$i]->price;
-	    $html .= "</td></tr>";
-
-    }
-    $html = "</table>";
+//	$html = "<table>";
+//    $ord = json_decode($el['ord']);
+//	for ($i = 0; $i < sizeof($ord); $i++) {
+//	    $html .= "<tr><td>";
+//	    $html .= $ord[$i]->name . ' ' . $ord[$i]->count . ' ' . $ord[$i]->price;
+//	    $html .= "</td></tr>";
+//
+//    }
+//    $html = "</table>";
     echo var_dump($ord);
   	foreach($data as $el) {?>
 	  <tr>	  	
@@ -83,7 +83,18 @@ else {
 	  		</table>
 	  	</td>
 	  	<td style="width: 180px !important; overflow: hidden; text-overflow: ellipsis; display: block;" title="<?=$el['adrs'];?>"><?=$el['adrs'];?></td>
-	  	<td><?//=?>fsdaf</td>
+	  	<td><?
+            $html = "<table>";
+            $ord = json_decode($el['ord']);
+            for ($i = 0; $i < sizeof($ord); $i++) {
+                $html .= "<tr><td>";
+                $html .= $ord[$i]->name . ' ' . $ord[$i]->count . ' ' . $ord[$i]->price;
+                $html .= "</td></tr>";
+
+            }
+            $html = "</table>";
+            echo $html;
+            ?>fsdaf</td>
 	  	<td width="56">
 	  	    <a class="tooltip" title="Содержимое" href="/<?=$URL[1];?>/<?=$URL[2];?>/<?=$URL[3];?>?item=<?=$el['id'];?>"><img src="/<?=$URL[1];?>/img/ico_photos.png"/></a>
 	  		<a class="tooltip" title="Редактировать" href="/<?=$URL[1];?>/<?=$URL[2];?>/<?=$URL[3];?>?edit=<?=$el['id'];?>"><img src="/<?=$URL[1];?>/img/ico_edit.png"/></a>
