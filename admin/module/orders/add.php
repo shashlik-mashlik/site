caef6bf7a5c02a2de1e81edd70b93dcd28a46292
MrHertz <rus.syndicate@gmail.com>
2017-03-07 13:35:38 +0300

    -

--- admin/module/orders/add.php
+++ admin/module/orders/add.php
@@ -1,10 +1,16 @@
 <?
 if($_POST['add']) {
 	$sql = "INSERT INTO `orders` SET
-		`about`    = '".mysql_real_escape_string($_POST['about'])."',
-		`name`    = '".mysql_real_escape_string($_POST['name'])."'";		
+		`sum`    = '".mysql_real_escape_string($_SESSION['cart_coast'])."',
+		`adrs`  =   '".mysql_real_escape_string(htmlspecialchars(stripslashes(substr($_POST['adrs'],0,32))))."',
+		`prsns`  =   '".mysql_real_escape_string(htmlspecialchars(stripslashes(substr($_POST['col'],0,32))))."',
+		`ord`  =   '".mysql_real_escape_string($basket)."',
+		`phone`  =   '".mysql_real_escape_string(htmlspecialchars(stripslashes(substr($_POST['tel'],0,32))))."',
+		`email`  =   '".mysql_real_escape_string(isset($email) ? $email : '')."',
+		`comment`  =   '".mysql_real_escape_string(htmlspecialchars(stripslashes(substr($_POST['message'],0,32))))."',
+		`name`    = '".mysql_real_escape_string(htmlspecialchars(stripslashes(substr($_POST['name'],0,32))))."'";
 	mysql_query($sql) OR die('DB ERROR: CAN\'T CREATE GALLERY');
-	header('Location: /'.$URL[1].'/'.$URL[2].'/'.$URL[3]);
-	exit;
+	//header('Location: /'.$URL[1].'/'.$URL[2].'/'.$URL[3]);
+	//exit;
 }
 ?>
