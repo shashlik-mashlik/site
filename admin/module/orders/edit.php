7cc48c9871060f65e869a3b41ef0a345d81e1e38
MrHertz <rus.syndicate@gmail.com>
2017-03-07 15:52:28 +0300

    -

--- admin/module/orders/edit.php
+++ admin/module/orders/edit.php
@@ -1,11 +1,11 @@
 <?
 echo "pep";
-$sql = "SELECT `status` FROM `orders` WHERE `id` = " . $_GET['id']);
-$r = mysql_fetch_assoc(mysql_query($sql));
-if ($r['status'] == 'ready') {
-    mysql_query("UPDATE `orders` SET `status` = 'expected'");
-} else {
-    mysql_query("UPDATE `orders` SET `status` = 'sent'");
-}
-Header('Location: /admin/module/orders');
+//$sql = "SELECT `status` FROM `orders` WHERE `id` = " . $_GET['id']);
+//$r = mysql_fetch_assoc(mysql_query($sql));
+//if ($r['status'] == 'ready') {
+//    mysql_query("UPDATE `orders` SET `status` = 'expected'");
+//} else {
+//    mysql_query("UPDATE `orders` SET `status` = 'sent'");
+//}
+//Header('Location: /admin/module/orders');
 ?>
