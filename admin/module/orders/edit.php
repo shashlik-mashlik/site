34e0bdd650e42930c52959a15b47d1f224128edd
MrHertz <rus.syndicate@gmail.com>
2017-03-07 15:55:11 +0300

    -

--- admin/module/orders/edit.php
+++ admin/module/orders/edit.php
@@ -1,9 +1,9 @@
 <?
 echo $_GET['id'];
-//$sql = "SELECT `status` FROM `orders` WHERE `id` = " . $_GET['id']);
-//$r = mysql_fetch_assoc(mysql_query($sql));
-//echo var_dump($r);
-echo mysql_ping();
+$sql = "SELECT `status` FROM `orders` WHERE `id` = " . $_GET['id'];
+$r = mysql_fetch_assoc(mysql_query($sql));
+echo var_dump($r);
+//echo mysql_ping();
 //if ($r['status'] == 'ready') {
 //    mysql_query("UPDATE `orders` SET `status` = 'expected'");
 //} else {
