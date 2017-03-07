8dde7159ee0e0c0cb9e7f1a2e9cd0425664e5e2f
MrHertz <rus.syndicate@gmail.com>
2017-03-07 14:12:37 +0300

    -

--- admin/module/orders/add.php
+++ admin/module/orders/add.php
@@ -1,12 +1,12 @@
 <?
 //if($_POST['add']) {
-$order = new Array();
+/*$order = new Array();
 foreach ($_SESSION['products'] as $key=>$value) {
     $q = "SELECT * FROM `mandarinko_catalog_item` WHERE id='" . $key . "'";
     $product = mysql_fetch_assoc(mysql_query($q));
     $order[] = ["id" => $key, "Наименование" => $product['name'], "Цена" => $product['price'], "Сумма" => $_SESSION['products'][$key]['count'] * $_SESSION['products'][$key]['coast'], "Количество" => $_SESSION['products'][$key]['count']];
 }
-
+*/
 	$sql = "INSERT INTO `orders` SET
 		`sum`    = '".mysql_real_escape_string($_SESSION['cart_coast'])."',
 		`adrs`  =   '".mysql_real_escape_string(htmlspecialchars(stripslashes(substr($_POST['adrs'],0,32))))."',
