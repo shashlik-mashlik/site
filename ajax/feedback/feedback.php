8c575619d36cb1924c14087b22479f4bb183c006
MrHertz <rus.syndicate@gmail.com>
2017-03-09 12:59:31 +0300

    Отзывы

--- ajax/feedback/feedback.php
+++ ajax/feedback/feedback.php
@@ -5,6 +5,18 @@
  * Date: 09.03.17
  * Time: 11:50
  */
+include $_SERVER['DOCUMENT_ROOT'] . '/ajax/config.php';
+/*
+$q = "SELECT `date`, `name`, `text` FROM `feedback` LIMIT 5";
+$r = mysql_fetch_assoc(mysql_query($q));
+$response = array();
+while ($row = $r) {
+    $response[] = ['text' => $row['text'], 'name' => $row['name'], 'date' => $row['date']];
+}
+echo json_encode($response);
+*/
 
-echo $_SERVER['HTTP_USER_AGENT'];
+$sql = "INSERT INTO `feedback` SET `name` = '" . $_POST['name'] . "', `email` = '" . $_POST['email'] . "', `text` = '" . $_POST['text'] . "', `phone` = '" . $_POST['phone'] . "'";
+$r = mysql_fetch_array(mysql_query($sql));
+echo json_encode("status" => true);
 ?>
