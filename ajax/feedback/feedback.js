eed4a93038d9525523ce0d6509e48ae375f10496
MrHertz <rus.syndicate@gmail.com>
2017-03-09 13:13:22 +0300

    Отзывы

--- ajax/feedback/feedback.js
+++ ajax/feedback/feedback.js
@@ -3,10 +3,10 @@
  */
 var feedback = {
     send: function() {
-        let query = 'name=' + document.querySelector('#ajax_name') +
-                    '&email=' + document.querySelector('#ajax_email') +
-                    '&phone=' + document.querySelector('#ajax_phone') +
-                    '&text=' + document.querySelector('#ajax_comment');
+        let query = 'name=' + document.querySelector('#ajax_name').value +
+                    '&email=' + document.querySelector('#ajax_email').value +
+                    '&phone=' + document.querySelector('#ajax_phone').value +
+                    '&text=' + document.querySelector('#ajax_comment').value;
         let xhr = ajax.ini();
         ajax.send(xhr, 'POST', '/ajax/feedback/feedback.php', query, feedback.handler);
     },
