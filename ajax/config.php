6afee280d1186a75996c4ddd866944f32423bfeb
MrHertz <rus.syndicate@gmail.com>
2017-03-09 18:12:07 +0300

    test

--- ajax/config.php
+++ ajax/config.php
@@ -1,9 +1,10 @@
 <?php
 
-$dbuser  = "antondtl_sm";
-$dbname  = "antondtl_sm";
-$dbpass  = "y3l0l3k0r";
-$dbhost  = "antondtl.beget.tech";
+//DATABASE
+$dbuser  = "shashlld_sm";
+$dbname  = "shashlld_sm";
+$dbpass  = "123456";//u
+$dbhost  = "localhost";
 
 mysql_connect($dbhost, $dbuser, $dbpass) or die ("Could not connect: ".mysql_error());
 mysql_select_db($dbname);
