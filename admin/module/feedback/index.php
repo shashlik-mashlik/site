740617d31bc88d6a7b1b6699579e3e523d750c9d
MrHertz <rus.syndicate@gmail.com>
2017-03-09 14:00:17 +0300

    -

--- admin/module/feedback/index.php
+++ admin/module/feedback/index.php
@@ -33,8 +33,8 @@ else {
 	<!--<p class="text_img"><img src="/<?=$URL[1];?>/img/ico_add.png"> <a href="/<?=$URL[1];?>/<?=$URL[2];?>/<?=$URL[3];?>?add=1">Добавить меню</a></p>-->
 	<table class="table" style="word-wrap: break-word;">
 	  <tr>
-	  	<th style="width: 300px !important;">О заказе</th> <!--номер, имя, тлф, кол-во персон -->
-	  	<th>Состав заказа</th>
+	  	<th style="width: 300px !important;">Информация</th> <!--номер, имя, тлф, кол-во персон -->
+	  	<th>Комментарий</th>
 	  	<th></th>
 	  </tr>
 	<?
@@ -47,28 +47,20 @@ else {
 	  	<td>
 	  		<table>
 	  			<tr>
-	  				<td style="color: #818181;">Номер заказа, статус:</td>
+	  				<td style="color: #818181;">Номер комментария:</td>
 	  				<td><?=$el['id'];?></td>
 	  			</tr>
-                <tr>
-                    <td style="color: #818181;">Статус:</td>
-                    <td><?=$el['status'] == 'ready' ? '<span style="color: red;">Ожидание</span >' : '<span style="color: green;">Готовится</span>';?></td>
-                </tr>
 	  			<tr>
 	  				<td style="color: #818181;">Имя:</td>
 	  				<td><?=$el['name'];?></td>
-	  			</tr>
-	  			<tr>
-	  				<td style="color: #818181;">Сумма:</td>
-	  				<td><?=$el['sum'];?></td>
 	  			</tr>
 	  			<tr>
 	  				<td style="color: #818181;">Телефон:</td>
 	  				<td><?=$el['phone'];?></td>
 	  			</tr>
 	  			<tr>
-	  				<td style="color: #818181;">Кол-во персон:</td>
-	  				<td><?=$el['prsns'];?></td>
+	  				<td style="color: #818181;">Почта:</td>
+	  				<td><?=$el['email'];?></td>
 	  			</tr>
 	  		</table>
 	  	</td>
