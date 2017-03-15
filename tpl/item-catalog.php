9639eaa0b350378e6110f37948357330f5b5516c
MrHertz <rus.syndicate@gmail.com>
2017-03-15 16:36:29 +0300

    photo

--- tpl/item-catalog.php
+++ tpl/item-catalog.php
@@ -48,7 +48,7 @@ $Ask = $json['query']['results']['rate']['Ask'];
 							<? if (file_exists('./upload/catalog/'.$el['id'].'_03.jpg')) {?><div class="item"><img src="<?='/upload/catalog/'.$el['id'].'_03.jpg'?>" alt="<?=$el['name']?>" title="<?=$el['name']?>"></div><?}?>
             </div>
             <div id="thumb-img" class="owl-carousel">
-              <div class="item"><img src="<?=$images_url?>" alt="<?=$el['name']?>" title="<?=$el['name']?>"></div>
+			  			<? if (file_exists('./upload/catalog/'.$el['id'].'_01.jpg')) {?><div class="item"><img src="<?=$images_url?>" alt="<?=$el['name']?>" title="<?=$el['name']?>"></div><?}?>
 						  <? if (file_exists('./upload/catalog/'.$el['id'].'_01.jpg')) {?><div class="item"><img src="<?='/upload/catalog/'.$el['id'].'_01.jpg'?>" alt="<?=$el['name']?>" title="<?=$el['name']?>"></div><?}?>
 							  <? if (file_exists('./upload/catalog/'.$el['id'].'_02.jpg')) {?><div class="item"><img src="<?='/upload/catalog/'.$el['id'].'_02.jpg'?>" alt="<?=$el['name']?>" title="<?=$el['name']?>"></div><?}?>
 								  <? if (file_exists('./upload/catalog/'.$el['id'].'_03.jpg')) {?><div class="item"><img src="<?='/upload/catalog/'.$el['id'].'_03.jpg'?>" alt="<?=$el['name']?>" title="<?=$el['name']?>"></div><?}?>
