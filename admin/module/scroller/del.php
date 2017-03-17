007eb55a2bfe17fa372ac3cdab7254749c408e49
MrHertz <rus.syndicate@gmail.com>
2017-03-17 16:08:55 +0300

    preloader

--- admin/module/scroller/del.php
+++
@@ -1,9 +0,0 @@
-<?
-if($_GET['delete']) {	$sql = "DELETE FROM `mandarinko_scroller` WHERE `id` = ".mysql_real_escape_string($_GET['delete']);
-	mysql_query($sql) OR die('DB ERROR: CAN\'T DEL scroller');
-	header('Location: /'.$URL[1].'/'.$URL[2].'/'.$URL[3]);
-	exit;
-}
-
-
-?>
