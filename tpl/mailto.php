5c108dffd4e22289a44d9241562033bb5dcd5d29
Anton Denisenko <milomory@gmail.com>
2017-03-20 12:33:51 +0300

    mail

--- tpl/mailto.php
+++ tpl/mailto.php
@@ -35,39 +35,59 @@
 $date_today = date("mdy");
 $primary_key = $primary_key."-".$date_today."-".time ();
 
-$email = $_POST['email'];
+//$email = $_POST['email'];
 $subject = "'SHASHLIK-MASHLIK' Заказ №: ".$primary_key." принят.";
-$message = "Имя заказчика: ".htmlspecialchars(stripslashes(substr($_POST['name'],0,32))).
-			"\nКол-во персон :" .htmlspecialchars(stripslashes(substr($_POST['col'],0,32))).
-			"\nАдрес: ".htmlspecialchars(stripslashes(substr($_POST['adrs'],0,32))). 
-			"\nтел.: ".htmlspecialchars(stripslashes(substr($_POST['tel'],0,32))). 
-			"\nКомментарий: ".htmlspecialchars(stripslashes(substr($_POST['message'],0,32)))."
-			".$basket."
-			\n__________________________________________________________
-			\nобщая итоговая сумма заказа.: ".$_SESSION['cart_coast']."
-			\n__________________________________________________________
-			\nномер заказа: ".$primary_key;
+$message = "
+
+	<html>
+	<head>
+	<title>SHASHLIK-MASHLIK</title>
+	</head>
+	<body>
+	<p>Заказ №: ".$primary_key." принят.</p>
+	<table>
+	<tr>
+	<th>Имя заказчика</th>
+	<th>Кол-во персон</th>
+	<th>Адрес</th>
+	<th>Телефон</th>
+	<th>Комментарий</th>
+	</tr>
+	<tr>
+	<td>".htmlspecialchars(stripslashes(substr($_POST['name'],0,32)))."</td>
+	<td>".htmlspecialchars(stripslashes(substr($_POST['col'],0,32)))."</td>
+	<td>".htmlspecialchars(stripslashes(substr($_POST['adrs'],0,32)))."</td>
+	<td>".htmlspecialchars(stripslashes(substr($_POST['tel'],0,32)))."</td>
+	<td>".htmlspecialchars(stripslashes(substr($_POST['message'],0,32)))."</td>
+	</tr>
+	</table>
+	<hr>
+	<p>общая итоговая сумма заказа.: ".$_SESSION['cart_coast']."</p>
+	<p>номер заказа: ".$primary_key."</p>
+	</body>
+	</html>
+	
+";
 
 $msg = "";
 
-
-	//$phonenumber=$_POST['tel'];
-	//$msg_sms="BONVIO-TRADE | Заказ №: ".$primary_key." принят.";
-	//include('tpl/register.php');//-------------------------------------------------------------------------------<<<а
+//$phonenumber=$_POST['tel'];
+//$msg_sms="BONVIO-TRADE | Заказ №: ".$primary_key." принят.";
+//include('tpl/register.php');//-------------------------------------------------------------------------------<<<а
 
 
 $headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8\r\n";
 //$headers .= "From:\r\n";
 /* дополнительные шапки */
-$headers .= "From: orders@shashlik-mashlik.com\r\n"; //" . $TEXT['email'] . "
+$headers .= "From: <".$TEXT['email'].">\r\n";
 
 //$headers .= "Cc: " . $TEXT['email'] . "\r\n";
 //$headers .= "Bcc: " . $TEXT['email'] . "\r\n";
-	if (mail($TEXT['email'], $subject, $message, $headers)){
-		@mail($email, $subject, $message, $headers );
-    	$msg = "<div class='alert alert-warning alert-dismissable'>
-			<b> Сообщение было отправлено! </b> &nbsp&nbsp&nbsp   Заказ # ".$primary_key."</div>";
+
+	if (mail($TEXT['email'].", ".$_POST['email'], $subject, $message, $headers)){
+		//@mail($email, $subject, $message, $headers );
+    	$msg = "<div class='alert alert-warning alert-dismissable'>Заказ # ".$primary_key." принят!</div>";
 	}
 	
 
