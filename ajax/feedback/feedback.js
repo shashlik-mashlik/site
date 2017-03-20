45cfae5b997f0aaaf1a490c02e6cfe82e313a3e3
Anton Denisenko <milomory@gmail.com>
2017-03-20 15:11:05 +0300

    fb

--- ajax/feedback/feedback.js
+++ ajax/feedback/feedback.js
@@ -10,14 +10,23 @@ var feedback = {
         let xhr = ajax.ini();
 
         if (!document.querySelector('#ajax_name').value) {
+            document.querySelector('#ajax_email_span').style.visibility = 'hidden';
+            document.querySelector('#ajax_phone_span').style.visibility = 'hidden';
+
             document.querySelector('#ajax_name_span').style.visibility = 'visible';
             document.querySelector('#ajax_name_span').innerHTML ='Введите Ваше имя';
         }
         else if (!document.querySelector('#ajax_email').value) {
+            document.querySelector('#ajax_name_span').style.visibility = 'hidden';
+            document.querySelector('#ajax_phone_span').style.visibility = 'hidden';
+
             document.querySelector('#ajax_email_span').style.visibility = 'visible';
             document.querySelector('#ajax_email_span').innerHTML ='Введите Ваш адрес';
         }
         else if (!document.querySelector('#ajax_phone').value) {
+            document.querySelector('#ajax_name_span').style.visibility = 'hidden';
+            document.querySelector('#ajax_email_span').style.visibility = 'hidden';
+
             document.querySelector('#ajax_phone_span').style.visibility = 'visible';
             document.querySelector('#ajax_phone_span').innerHTML ='Введите номер телефона';
         }
