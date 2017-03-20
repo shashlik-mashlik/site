dff6ef1404d59403dd051386927c1389ce519b1e
Anton Denisenko <milomory@gmail.com>
2017-03-20 15:31:44 +0300

    logo

--- tpl/header.php
+++ tpl/header.php
@@ -20,7 +20,7 @@ session_start();// Начинаем сессию
             <a href="/" class="light-logo"><img src="/themes/majesty/img/logo-intro-dark-loader.png" alt="Logo" height="100px"></a>
             <a href="/" class="dark-logo"><img src="/themes/majesty/img/logo-intro-dark-loader.png" alt="Logo" height="70px"></a>
         </div>
-        <div style="float: left; height: 80px; line-height: 80px; font-size: 22px; color: white;" class="hidden-xs">тел. <?=$TEXT['phone']?></div>
+        <div style="float: left; height: 90px; line-height: 90px; font-size: 22px; color: white;" class="hidden-xs">тел. <?=$TEXT['phone']?></div>
                     <!--End #logo  -->
         <!-- Primary Navigation
                     ============================================= -->
