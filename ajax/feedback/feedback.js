e94d05761fcc188f0a5644d0b4dd098738b54b00
Anton Denisenko <milomory@gmail.com>
2017-03-20 14:53:46 +0300

    fb

--- ajax/feedback/feedback.js
+++ ajax/feedback/feedback.js
@@ -2,16 +2,26 @@
  * Created by kilohertz on 09.03.17.
  */
 var feedback = {
-    send: function() {
+    send: function () {
         let query = 'name=' + document.querySelector('#ajax_name').value +
-                    '&email=' + document.querySelector('#ajax_email').value +
-                    '&phone=' + document.querySelector('#ajax_phone').value +
-                    '&text=' + document.querySelector('#ajax_comment').value;
+            '&email=' + document.querySelector('#ajax_email').value +
+            '&phone=' + document.querySelector('#ajax_phone').value +
+            '&text=' + document.querySelector('#ajax_comment').value;
         let xhr = ajax.ini();
-        ajax.send(xhr, 'POST', '/ajax/feedback/feedback.php', query, feedback.handler);
+        if (!document.querySelector('#ajax_phone').value) {
+            alert('Введите номер телефона');
+        }
+        else {
+            ajax.send(xhr, 'POST', '/ajax/feedback/feedback.php', query, feedback.handler);
+        }
+
+
     },
-    handler: function(param) {
+    handler: function (param) {
         let response = eval("(" + param.responseText + ")");//
-        if (response.status == true) {alert('Ваш комментарий добавлен. Он появится в списке после проверки.'); window.location.reload();}
+        if (response.status == true) {
+            alert('Ваш комментарий добавлен. Он появится в списке после проверки.');
+            window.location.reload();
+        }
     }
 }
