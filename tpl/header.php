c023b4dbb3f946dcd762dd66c9b88d4947c20695
MrHertz <rus.syndicate@gmail.com>
2017-03-21 17:15:22 +0300

    test caurusel

--- tpl/header.php
+++ tpl/header.php
@@ -34,7 +34,7 @@ session_start();// Начинаем сессию
                 <ul>
                   <li class="mega-menu-title">
                     <div id="menu_carousel">
-												<span style="height: 100%; width: 30px; background-color: red;">-</span>
+												<div style="height: 100%; width: 30px; background-color: red; position: absolute; margin-left: 0px;">-</div>
 											<?
 												$q="SELECT * FROM `mandarinko_presentation_item`";
 												$result = mysql_query($q);
